74e9ff038ecf666c335a0456d9bf9e82fd1d2710
pat <patrick.philipt@gmail.com>
2020-09-19 10:22:51 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by patrick philipot on 19/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
